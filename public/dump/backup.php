27d92c2bae8545b25b2184ceef03ec3b49236fa0
zcorpse <zcorpse@163.com>
2014-11-28 09:08:32 +0800

    delete files

--- public/dump/backup.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'dbmanage.php';
-
-$db = new DBManage ( 'localhost', 'root', 'M9CBCtGJtH', 'hms_database', 'utf8' );
-// 参数：备份哪个表(可选),备份目录(可选，默认为backup),分卷大小(可选,默认2000，即2M)
-$db->backup ();
-
-?>
--- public/dump/restore.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'dbmanage.php';
-
-$db = new DBManage ( 'localhost', 'root', 'nirvana', 'hlbs', 'utf8' );
-//参数：sql文件
-$db->restore ( './backup/20141026031635_all_v1.sql');
-
-?>
